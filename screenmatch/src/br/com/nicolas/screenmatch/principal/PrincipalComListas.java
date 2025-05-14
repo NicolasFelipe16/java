802e6cf779a6ac5d09c13705fd27b8ed03aeb72d
Nícolas Felipe <nicolas.felipef16@gmail.com>
2025-05-14 19:01:01 -0300

    Ordenação de listas com 'Colletions.sort' e utilidade da interface 'Comparable' para essa comparação.

--- screenmatch/src/br/com/nicolas/screenmatch/principal/PrincipalComListas.java
+++ screenmatch/src/br/com/nicolas/screenmatch/principal/PrincipalComListas.java
@@ -5,6 +5,8 @@ import br.com.nicolas.screenmatch.modelos.Serie;
 import br.com.nicolas.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -29,9 +31,24 @@ public class PrincipalComListas {
             if (item instanceof Filme filme && filme.getClassificacao() > 2) {
                 System.out.println("Classificação: " + filme.getClassificacao());
             }
-
-
-
         }
+
+        ArrayList<String> buscaPorArtistas = new ArrayList<>();
+        buscaPorArtistas.add("Adam Sandler");
+        buscaPorArtistas.add("Paulo");
+        buscaPorArtistas.add("João");
+
+        System.out.println(buscaPorArtistas);
+
+        Collections.sort(buscaPorArtistas);
+        System.out.println("Depois da ordenação:");
+        System.out.println(buscaPorArtistas);
+
+        Collections.sort(lista);
+        System.out.println("Lista ordenada por nomes:");
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Lista ordenada por ano:");
+        System.out.println(lista);
     }
 }
